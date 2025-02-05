b22abaff1ff82007da3b0dab9f4692ba9866fbf9
ondrahladik <ondra@ok1kky.cz>
2025-02-05 21:05:54 +0100

    Edit file

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ RELAY_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 
-# Relé je LOW aktivní
-GPIO.output(RELAY_PIN, GPIO.HIGH)
+# Relé je aktivní při LOW
+GPIO.output(RELAY_PIN, GPIO.LOW)
 
 def reset_device():
     print("Reset device")
-    GPIO.output(RELAY_PIN, GPIO.LOW)  # Sepnutí relé 
+    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Sepnutí relé 
     time.sleep(1)  # Doba resetu
-    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Vypnutí relé 
+    GPIO.output(RELAY_PIN, GPIO.LOW)  # Vypnutí relé 
     print("Reset complete")
 
 try:
     while True:
-        time.sleep(120)  # Počkej 2 minuty
+        time.sleep(30)  # Počkej 2 minuty
         reset_device()   # Proveď reset
 
 except KeyboardInterrupt:
