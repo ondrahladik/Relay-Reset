bc3e6675b3c4d292d97d70e9dc863670b479375f
ondrahladik <ondra@ok1kky.cz>
2025-02-05 20:34:27 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO pin pro relé
+RELAY_PIN = 17  
+
+# Nastavení GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+# Relé je LOW aktivní
+GPIO.output(RELAY_PIN, GPIO.HIGH)
+
+def reset_device():
+    print("Reset zařízení...")
+    GPIO.output(RELAY_PIN, GPIO.LOW)  # Sepnutí relé 
+    time.sleep(1)  # Doba resetu
+    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Vypnutí relé 
+    print("Reset dokončen")
+
+try:
+    while True:
+        time.sleep(120)  # Počkej 2 minuty
+        reset_device()   # Proveď reset
+
+except KeyboardInterrupt:
+    print("Ukončuji program...")
+    GPIO.cleanup()  # Uvolnění GPIO pinů
