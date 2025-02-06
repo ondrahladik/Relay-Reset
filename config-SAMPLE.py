b0aa697d408201754c1ab36f6ef2801ab2a829e7
ondrahladik <ondra@ok1kky.cz>
2025-02-06 22:44:30 +0100

    Update config

--- config-SAMPLE.py
+++ config-SAMPLE.py
@@ -1,6 +1,6 @@
 RELAY_PIN = 17
 RELAY_DELAY = 2  # Activation delay for relay in seconds
-RESET_TIME = "" # Time of day to reset the device
+RESET_TIME = "16:00" # Time of day to reset the device
 
 MQTT_ACTIVE = False  # Enable MQTT
 MQTT_BROKER = ""  # MQTT broker
