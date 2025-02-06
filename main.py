6cff61f6b52464098246783e2884c70d2416ef22
ondrahladik <ondra@ok1kky.cz>
2025-02-06 17:41:48 +0100

    Add MQTT_ACTIVE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import schedule
 import paho.mqtt.client as mqtt
 from reset import reset_device
-from config import RESET_TIME, MQTT_BROKER, MQTT_PORT, MQTT_TOPIC, MQTT_USERNAME, MQTT_PASSWORD
+from config import RESET_TIME, MQTT_ACTIVE, MQTT_BROKER, MQTT_PORT, MQTT_TOPIC, MQTT_USERNAME, MQTT_PASSWORD
 
 # Umlčování DeprecationWarning
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -17,25 +17,27 @@ def on_message(client, userdata, msg):
     if msg.payload.decode() == "reset":
         reset_device()
 
-# Inicializace MQTT klienta
-client = mqtt.Client(client_id="mqtt_client", protocol=mqtt.MQTTv5)
+# Inicializace MQTT klienta pouze pokud je MQTT aktivní
+if MQTT_ACTIVE:
+    client = mqtt.Client(client_id="mqtt_client", protocol=mqtt.MQTTv5)
 
-# Přidání autentifikace (pokud je potřeba)
-if MQTT_USERNAME and MQTT_PASSWORD:
-    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    # Přidání autentifikace (pokud je potřeba)
+    if MQTT_USERNAME and MQTT_PASSWORD:
+        client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
-client.on_message = on_message
+    client.on_message = on_message
 
-# Připojení k MQTT brokeru
+    # Připojení k MQTT brokeru
+    try:
+        client.connect(MQTT_BROKER, MQTT_PORT, 60)
+        client.subscribe(MQTT_TOPIC)
+    except Exception as e:
+        print(f"Chyba při připojování k MQTT brokeru: {e}")
+        exit(1)
+
 try:
-    client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    client.subscribe(MQTT_TOPIC)
-except Exception as e:
-    print(f"Chyba při připojování k MQTT brokeru: {e}")
-    exit(1)
-
-try:
-    client.loop_start()
+    if MQTT_ACTIVE:
+        client.loop_start()
 
     while True:
         schedule.run_pending()
@@ -45,5 +47,6 @@ except KeyboardInterrupt:
     print("Exiting the program...")
 
 finally:
-    client.loop_stop()
-    GPIO.cleanup()  
+    if MQTT_ACTIVE:
+        client.loop_stop()
+    GPIO.cleanup()
