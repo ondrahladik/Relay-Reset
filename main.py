896ed5a093c6330ec6cea0aabb6b2deaa950b233
ondrahladik <ondra@ok1kky.cz>
2025-02-06 17:04:04 +0100

    Edit code

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import warnings
 import RPi.GPIO as GPIO
 import time
+import schedule
 import paho.mqtt.client as mqtt
 from reset import reset_device
-from config import MQTT_BROKER, MQTT_PORT, MQTT_TOPIC
+from config import RESET_TIME, MQTT_BROKER, MQTT_PORT, MQTT_TOPIC, MQTT_USERNAME, MQTT_PASSWORD
 
 # Umlčování DeprecationWarning
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Naplánování spuštění každý den v RESET_TIME
+schedule.every().day.at(RESET_TIME).do(reset_device)
 
 # Funkce pro zpracování přijaté zprávy
 def on_message(client, userdata, msg):
@@ -15,22 +19,31 @@ def on_message(client, userdata, msg):
 
 # Inicializace MQTT klienta
 client = mqtt.Client(client_id="mqtt_client", protocol=mqtt.MQTTv5)
+
+# Přidání autentifikace (pokud je potřeba)
+if MQTT_USERNAME and MQTT_PASSWORD:
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+
 client.on_message = on_message
 
 # Připojení k MQTT brokeru
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
-
-# Přihlášení k topicu
-client.subscribe(MQTT_TOPIC)
+try:
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    client.subscribe(MQTT_TOPIC)
+except Exception as e:
+    print(f"Chyba při připojování k MQTT brokeru: {e}")
+    exit(1)
 
 try:
-    # Start MQTT klienta v samostatném vlákně
     client.loop_start()
 
     while True:
-        time.sleep(60)
-        reset_device()
+        schedule.run_pending()
+        time.sleep(10)
 
 except KeyboardInterrupt:
     print("Exiting the program...")
+
+finally:
     client.loop_stop()
+    GPIO.cleanup()  
--- reset.py
+++ reset.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-from config import RELAY_PIN
+from config import RELAY_PIN, RELAY_DELAY
 
 def reset_device():
     print("Reset device")
@@ -8,6 +8,6 @@ def reset_device():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RELAY_PIN, GPIO.OUT)
     GPIO.output(RELAY_PIN, GPIO.HIGH)
-    time.sleep(2)
+    time.sleep(RELAY_DELAY)
     GPIO.cleanup() 
     print("Reset complete")
