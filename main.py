2baafd30719e0a93673ebb0286876421fe0a7157
ondrahladik <ondra@ok1kky.cz>
2025-02-07 23:44:17 +0100

    Add reset device, all

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-import warnings
-import RPi.GPIO as GPIO
 import time
 import schedule
+import warnings
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
-from reset import reset_device
+from reset import reset_relay, reset_device, reset_all
 from config import RESET_TIME, MQTT_ACTIVE, MQTT_BROKER, MQTT_PORT, MQTT_TOPIC, MQTT_USERNAME, MQTT_PASSWORD
 
 # Umlčování DeprecationWarning
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Naplánování spuštění každý den v RESET_TIME
-schedule.every().day.at(RESET_TIME).do(reset_device)
+# Schedule to run every day at RESET_TIME
+schedule.every().day.at(RESET_TIME).do(reset_relay)
 
-# Funkce pro zpracování přijaté zprávy
+# Function to process the received message
 def on_message(client, userdata, msg):
-    if msg.payload.decode() == "reset":
-        reset_device()
+    payload = msg.payload.decode()
 
-# Inicializace MQTT klienta pouze pokud je MQTT aktivní
+    if payload == "reset-relay":
+        reset_relay()
+    elif payload == "reset-device":
+        reset_device()
+    elif payload == "reset-all":
+        reset_all()
+    else:
+        print(f"Unknown command: {payload}")
+
+# Initialize the MQTT client only if MQTT is active
 if MQTT_ACTIVE:
     client = mqtt.Client(client_id="mqtt_client", protocol=mqtt.MQTTv5)
 
-    # Přidání autentifikace (pokud je potřeba)
+    # Add authentication (if needed)
     if MQTT_USERNAME and MQTT_PASSWORD:
         client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
     client.on_message = on_message
 
-    # Připojení k MQTT brokeru s opakováním pokusů při neúspěchu
+    # Connection to the MQTT broker with retries on failure
     while True:
         try:
             client.connect(MQTT_BROKER, MQTT_PORT, 60)
             client.subscribe(MQTT_TOPIC)
-            break  # Připojení bylo úspěšné, break z opakovací smyčky
+            break  
         except Exception as e:
-            print(f"Chyba při připojování k MQTT brokeru: {e}. Pokusím se znovu za 5 sekund.")
+            print(f"Error connecting to MQTT broker: {e}. I will try again in 5 seconds.")
             time.sleep(5)
 
 try:
--- reset.py
+++ reset.py
@@ -1,6 +1,7 @@
-import RPi.GPIO as GPIO
+import os
 import time
 import logging
+import RPi.GPIO as GPIO
 from config import RELAY_PIN, RELAY_DELAY
 
 logging.basicConfig(
@@ -12,8 +13,9 @@ logging.basicConfig(
     ]
 )
 
-def reset_device():
-    logging.info("Reset device")  
+# Function to reset the relay
+def reset_relay():
+    logging.info("Reset relay")  
 
     try:
         GPIO.setmode(GPIO.BCM)
@@ -21,7 +23,19 @@ def reset_device():
         GPIO.output(RELAY_PIN, GPIO.HIGH)
         time.sleep(RELAY_DELAY)
     except Exception as e:
-        logging.error(f"Chyba při resetování: {e}")
+        logging.error(f"Reset error: {e}")
     finally:
         GPIO.cleanup()
         logging.info("Reset complete")  
+
+# Function to reset the device
+def reset_device():
+    logging.info("Reset device")  
+    os.system("sudo reboot")
+
+# Function to reset all (relay and device)
+def reset_all():
+    logging.info("Reset all")  
+    reset_relay()
+    time.sleep(2)
+    reset_device()
